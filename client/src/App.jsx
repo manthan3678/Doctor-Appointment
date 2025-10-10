38466cc739b2b088538424de979538c784b2da62
manthan3678 <manthangedam198@gmail.com>
2025-10-10 20:30:21 +0530

    Intial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-body-secondary text-decoration-underline ">
+        Manthan Gedam
+      </h1>
+    </div>
+  );
+};
+
+export default App;
