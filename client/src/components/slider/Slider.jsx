b968322a4e578cbd31f8a8865b49386fab35923b
manthan3678 <manthangedam198@gmail.com>
2025-10-12 15:13:25 +0530

    Home Page Slider

--- client/src/components/slider/Slider.jsx
+++ client/src/components/slider/Slider.jsx
@@ -1,11 +1,43 @@
-import React from 'react'
-
+import React from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay } from "swiper/modules";
+// Import Swiper styles
+import "swiper/css";
+import BannerOne from "../../assets/Gemini_Generated_Image_6.png";
+import BannerTwo from "../../assets/Gemini_Generated_Image_2.png";
+import BannerThree from "../../assets/Gemini_Generated_Image_3.png";
+import Bannerfour from "../../assets/Gemini_Generated_Image_4.png";
+import Bannerfive from "../../assets/Gemini_Generated_Image_5.png";
+import { data } from "react-router";
 const Slider = () => {
+  const images = [BannerOne, BannerTwo, BannerThree, Bannerfour, Bannerfive];
   return (
     <>
-      
+      <Swiper
+        modules={[Autoplay]}
+        spaceBetween={10}
+        slidesPerView={1}
+        loop={true}
+        autoplay={{ delay: 3000, disableOnInteraction: false }}
+      >
+        {images.map((d, i) => (
+          <SwiperSlide key={i}>
+            <img
+              src={d}
+              alt="bannerImages"
+              style={{
+                height: "100vh",
+                width: "100%",
+                objectFit: "cover",
+                objectPosition: "center",
+              }}
+            />
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </>
-  )
-}
+  );
+};
 
-export default Slider
+export default Slider;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from "react";
+import Slider from "../components/slider/Slider";
 
 const Home = () => {
   return (
-    <div>
-      Home
-    </div>
-  )
-}
+    <>
+      {/* Slider */}
+      <Slider />
+    </>
+  );
+};
 
-export default Home
+export default Home;
