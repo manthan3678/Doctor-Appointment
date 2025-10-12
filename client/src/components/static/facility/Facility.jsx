5056ef5f047db6681661229a52e558d81da29010
manthan3678 <manthangedam198@gmail.com>
2025-10-12 19:15:09 +0530

    Facility Section Updated

---
+++ client/src/components/static/facility/Facility.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./facility.css";
+import FacilityData from "./facility-data.json";
+const Facility = () => {
+  // console.log(FacilityData)
+  return (
+    <>
+      <h2 className="facilityHeadig">Facilities</h2>
+      <div className="facility-container">
+        {FacilityData.map((d, i) => (
+          <div className="card" key={i}>
+            <i className={`${d.icon} card-img-top`}></i>
+            <div className="card-body">
+              <h5 className="card-title">{d.title}</h5>
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default Facility;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Slider from "../components/slider/Slider";
-
+import Facility from "../components/static/facility/Facility";
 const Home = () => {
   return (
     <>
       {/* Slider */}
       <Slider />
+      {/* Facility Section */}
+      <Facility />
     </>
   );
 };
