b004dc6dabed956cda27244d8556cc2e32615a7d
manthan3678 <manthangedam198@gmail.com>
2025-12-28 21:46:40 +0530

    Server

---
+++ server/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+// res object
+const app = express();
+
+//route
+app.get("/", (req, res) => {
+  res.send("<h2>Port Running On 8080</h2>");
+});
+
+const PORT = 8080;
+// run server
+app.listen(PORT, () => {
+  console.log("SERVER RUNNING ON localhost:8080");
+});
